5cf0c5be4f970974ffab97c20f0e829fec0ce049
mohammedfaizaansharief1 <mohdfaizaansharief@gmail.com>
2023-12-19 19:20:19 +0530

    6_react

--- REACT/vite-project/src/App.jsx
+++ REACT/vite-project/src/App.jsx
@@ -2,13 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Child from './Child'
+import Child1 from './Child1'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
+  let obj={
+    name:"Faizaan",
+    age:"20"
+  }
+
+  let a=20
+  // function fun(){
+  //   if(a%2==0){
+  //     console.log("Even")
+  //   }
+  //   else{
+  //     console.log("Odd")
+  //   }
+  // }
   return (
+    
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +44,11 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
+      <Child {...obj} /><br></br>
+      {/* <Child1 /> */}
+      {a%2==0 ? <Child1/> :<p>No</p>}
     </>
   )
 }
