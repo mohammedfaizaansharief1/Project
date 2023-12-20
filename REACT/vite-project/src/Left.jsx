dbacde5b3ccc74e025f3de2a74781983c6733245
mohammedfaizaansharief1 <mohdfaizaansharief@gmail.com>
2023-12-21 04:31:30 +0530

    Final_design

--- REACT/vite-project/src/Left.jsx
+++ REACT/vite-project/src/Left.jsx
@@ -5,9 +5,9 @@ import Form from './Form'
 export default function Left() {
   return (
     <div className='left'>
-      <h4>Travling!</h4>
+      <h2 className='h2'>Travling!</h2>
       <Form />
-      <p>&#169; 2021 Travling.All Rights Reserved.</p>
+      <p className='para'>&#169; 2021 Travling.All Rights Reserved.</p>
     </div>
   )
 }
--- REACT/vite-project/src/Right.jsx
+++ REACT/vite-project/src/Right.jsx
@@ -1,10 +1,11 @@
 // import React from 'react'
 import './Right.css'
+import form2 from './assets/form2.png'
 
 export default function Right() {
   return (
     <div className='right'>
-      ji hi
+      <img src={form2} alt="" />
     </div>
   )
 }
