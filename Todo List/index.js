06c8995a9db482682eabec5c61a884f8ef35727e
mohammedfaizaansharief1 <mohdfaizaansharief@gmail.com>
2023-12-24 01:29:28 +0530

    design changed V2

--- Todo List/index.js
+++ Todo List/index.js
@@ -5,8 +5,9 @@ let btn = document.querySelector('button')
 
 let data = []
 window.onload = ()=>{
+    lower.innerHTML = `Your Task's`
     data = JSON.parse(localStorage.getItem('data')) || []
-    let item = data.map((e)=>{
+    let item = data.map((e,i)=>{
         let item = `<div class="item">
                         <div class="left">
                             <input type="checkbox" name="" id="">
@@ -16,7 +17,7 @@ window.onload = ()=>{
                             </div>
                         </div>
                         <div class="right">
-                            <i class="ri-delete-bin-5-fill"></i>
+                            <i class="ri-delete-bin-5-fill" onclick="deletee(${i})"></i>
                         </div>
                     </div>`
         return item
@@ -33,9 +34,37 @@ inp2.addEventListener('keyup',(e)=>{
     i2 = e.target.value
 })
 btn.addEventListener('click',(e)=>{
-    data.push({title:i1,desc:i2})
-    localStorage.setItem('data',JSON.stringify(data))
-    let item =  data.map((e)=>{
+    if(i1=="" && i2==""){
+        alert("Please Enter the details")
+    }    
+    else{
+        data.push({title:i1,desc:i2})
+        localStorage.setItem('data',JSON.stringify(data))
+        let item =  data.map((e,i)=>{
+            let item = `<div class="item">
+                            <div class="left">
+                                <input type="checkbox" name="" id="">
+                                <div class="left-in">
+                                    <p class="title">${e.title}</p>
+                                    <p>${e.desc}</p>
+                                </div>
+                            </div>
+                            <div class="right">
+                                <i class="ri-delete-bin-5-fill" onclick="deletee(${i})"></i>
+                            </div>
+                        </div>`
+            return item
+        })
+        lower.innerHTML = item.join('')
+        }
+        inp1.value=""
+        inp2.value=""
+})  
+
+function deletee(i){
+    data.splice(i,1)
+    localStorage.setItem("data",JSON.stringify(data))
+    let item =  data.map((e,i)=>{
         let item = `<div class="item">
                         <div class="left">
                             <input type="checkbox" name="" id="">
@@ -45,13 +74,10 @@ btn.addEventListener('click',(e)=>{
                             </div>
                         </div>
                         <div class="right">
-                            <i class="ri-delete-bin-5-fill"></i>
+                            <i class="ri-delete-bin-5-fill" onclick="deletee(${i})"></i>
                         </div>
                     </div>`
         return item
     })
     lower.innerHTML = item.join('')
-    inp1.value=""
-    inp2.value=""
-})  
-
+}
